89924b19f9d18febfeb1f4f5ae6819cf8931e48c
ajulayfi <ajulayfi@gmail.com>
2019-12-09 22:31:04 -0600

    Edit Student Class

--- Student Exam Report Generator/src/Student.java
+++ Student Exam Report Generator/src/Student.java
@@ -1,4 +1,28 @@
 
 public class Student {
 
+	private String firstname;
+	private String lastname;
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Student(String firstname, String lastname) {
+		setFirstname(firstname);
+		setLastname(lastname);
+	}
+
 }

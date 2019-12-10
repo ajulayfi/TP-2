60ab4089838e8acf0dae79c0c5022324a8390c48
alju3541 <alju3541@OSS230-08.stthomas.edu>
2019-12-09 23:23:52 -0600

    create class "Student" + Edit class "Class"

--- Student Exam Report Generator/src/Class.java
+++ Student Exam Report Generator/src/Class.java
@@ -1,12 +1,34 @@
 public class Class {
-private int id;
-private int studentQuantity;
+	/*
+	 * Instance Variables
+	 */
+	private int classID;
+	private int studentQuantity;
+	private Student[] students;
 
-public int getClassid() {
-    return id;
-  }
-public void setClassid() {
-	
-  }
 
+
+	public int getClassID() {
+		return classID;
+	}
+
+	public void setClassID(int classID) {
+		this.classID = classID;
+	}
+
+	public int getStudentQuantity() {
+		return studentQuantity;
+	}
+
+	public void setStudentQuantity(int studentQuantity) {
+		this.studentQuantity = studentQuantity;
+	}
+
+	public Student[] getStudents() {
+		return students;
+	}
+
+	public void setStudents(Student[] students) {
+		this.students = students;
+	}
 }
